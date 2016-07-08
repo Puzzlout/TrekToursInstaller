f3208d8b648f6c0d47152c768a7695286f897751
Stephane Charoy <trektours.pm@gmail.com>
2016-07-08 16:20:43 +0000

    Setup server script update

--- setup-server.sh
+++ setup-server.sh
@@ -4,3 +4,7 @@ git clone https://github.com/Puzzlout/TrekToursFlyer.git
 mv TrekToursFlyer public
 git clone https://github.com/Puzzlout/TrekToursApi.git
 mv TrekToursApi api
+sh deploy/deploy.sh
+sh deploy/php-upgrade.sh 7.0.3
+sh deploy/Projects/TrekTours/Api/install.sh prod
+sh deploy/Projects/TrekTours/Flyer/install.sh prod
