036ab7dc4105b24744060b3bed465d493921c934
Stephane Charoy <trektours.pm@gmail.com>
2016-07-10 16:10:37 +0000

    updating the installer

--- setup-server.sh
+++ setup-server.sh
@@ -1,10 +1,30 @@
+################################################################################
+# Cloning the repositories
+################################################################################
+echo "Cloning the repositories..."
 git clone https://github.com/Puzzlout/CloudDeploy.git
+git clone https://github.com/Puzzlout/Puzzlout/TrekToursFlyer.git
+git clone https://github.com/Puzzlout/Puzzlout/TrekToursApi.git
+################################################################################
+# Renaming cloned repositories
+################################################################################
+echo "Renaming cloned repositories..."
 mv CloudDeploy deploy
-git clone https://github.com/Puzzlout/TrekToursFlyer.git
 mv TrekToursFlyer public
-git clone https://github.com/Puzzlout/TrekToursApi.git
 mv TrekToursApi api
+################################################################################
+# Updating the server
+################################################################################
+echo "Updating the server"
 sh deploy/deploy.sh
+################################################################################
+# Updating the PHP version and server
+################################################################################
+echo "Updating the PHP version and server..."
 sh deploy/php-upgrade.sh 7.0.3
+################################################################################
+# Installing the applications
+################################################################################
+echo "Installing the applications"
 sh deploy/Projects/TrekTours/Api/install.sh prod
 sh deploy/Projects/TrekTours/Flyer/install.sh prod
