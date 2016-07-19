eb7b5e03163598122f3f4ab7df8d779ff1182289
Jérémie Litzler <contact@jeremielitzler.net>
2016-07-19 07:18:50 +0200

    #1

--- first-install-on-c9.sh
+++ first-install-on-c9.sh
@@ -21,10 +21,10 @@ sh deploy/deploy.sh
 # Updating the PHP version and server
 ################################################################################
 echo "Updating the PHP version and server..."
-bash deploy/php-upgrade.sh 7.0.1
+bash deploy/php-upgrade.sh 7.0.8
 ################################################################################
 # Installing the applications
 ################################################################################
 echo "Installing the applications"
-sh deploy/Projects/TrekTours/Api/install.sh prod
-sh deploy/Projects/TrekTours/Flyer/install.sh prod
+sh deploy/Projects/TrekTours/Api/install-c9.sh prod
+sh deploy/Projects/TrekTours/Flyer/install-c9.sh prod
--- update-applications-c9.sh
+++ update-applications-c9.sh
@@ -1,30 +1,11 @@
 ################################################################################
-# Cloning the repositories
-################################################################################
-echo "Cloning the repositories..."
-git clone https://github.com/Puzzlout/CloudDeploy.git
-git clone https://github.com/Puzzlout/TrekToursFlyer.git
-git clone https://github.com/Puzzlout/TrekToursApi.git
-################################################################################
-# Renaming cloned repositories
-################################################################################
-echo "Renaming cloned repositories..."
-mv CloudDeploy deploy
-mv TrekToursFlyer public
-mv TrekToursApi api
-################################################################################
 # Updating the server
 ################################################################################
 echo "Updating the server"
 sh deploy/deploy.sh
 ################################################################################
-# Updating the PHP version and server
-################################################################################
-echo "Updating the PHP version and server..."
-bash deploy/php-upgrade.sh 7.0.1
-################################################################################
 # Installing the applications
 ################################################################################
 echo "Installing the applications"
-sh deploy/Projects/TrekTours/Api/install.sh prod
-sh deploy/Projects/TrekTours/Flyer/install.sh prod
+sh deploy/Projects/TrekTours/Api/update-c9.sh prod
+sh deploy/Projects/TrekTours/Flyer/update-c9.sh prod
